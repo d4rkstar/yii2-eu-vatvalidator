dc4035d27c0e89546d2789196c6185ba155ff13a
vlad.nedelcu@gmail.com <vlad.nedelcu@gmail.com>
2016-03-04 11:32:12 +0200

    initial commit

---
+++ EuVatValidator.php
@@ -0,0 +1,85 @@
+<?php
+namespace elvenpath\yii2_eu_vatvalidator;
+
+use \SoapClient;
+use \Exception;
+use \yii\validators\Validator;
+
+/**
+ * Class EuVatValidator
+ *
+ * @package elvenpath\yii2_eu_vatvalidator
+ */
+class EuVatValidator extends Validator
+{
+    /** @var string */
+    public $wsdl_uri = "http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl";
+    /** @var string */
+    public $country_code;
+    /**
+     * @var bool
+     *
+     * If true the $model_name_attribute and $model_address_attribute will be populated
+     * with the EU official company name and address
+     */
+    public $populate_model = false;
+    /** @var string */
+    public $model_name_attribute = 'name';
+    /** @var string */
+    public $model_address_attribute = 'address';
+    /** @var SoapClient */
+    private $client;
+
+    public function init()
+    {
+        parent::init();
+
+        if (!class_exists('SoapClient')) {
+            throw new Exception('The Soap library has to be installed and enabled');
+        }
+
+        $this->client = new SoapClient($this->wsdl_uri, ['trace' => true]);
+    }
+
+    /**
+     * @param \yii\base\Model $model
+     * @param string $attribute
+     * @return bool
+     */
+    public function validateAttribute($model, $attribute)
+    {
+        if (!in_array($model->$attribute, ['USA', 'Web'])) {
+            $this->addError($model, $attribute, 'The country must be either "USA" or "Web".');
+        }
+
+        /** @noinspection PhpUndefinedMethodInspection */
+        $rs = $this->client->checkVat(['countryCode' => $this->country_code, 'vatNumber' => $model->$attribute]);
+        if (!$rs->valid) {
+            $this->addError($model, $attribute, $this->message);
+        } else {
+            if ($this->populate_model) {
+                $this->model_name_attribute = $this->cleanUpString($rs->name);
+                $this->model_address_attribute = $this->cleanUpString($rs->address);
+            }
+        }
+    }
+
+    private function cleanUpString($string)
+    {
+        for ($i = 0; $i < 100; $i++) {
+            $newString = str_replace("  ", " ", $string);
+            if ($newString === $string) {
+                break;
+            } else {
+                $string = $newString;
+            }
+        }
+
+        $newString = "";
+        $words = explode(" ", $string);
+        foreach ($words as $k => $w) {
+            $newString .= ucfirst(strtolower($w)) . " ";
+        }
+        return $newString;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# yii2-eu-vatvalidator
 A Yii validator for EU VAT numbers
+
+# EU VAT validator for Yii2 #
+
+based on https://github.com/herdani/vat-validation
+
+## About ##
+The
+- Validate a VAT number
+- Retrieve information like the name or the address of the company
+
+The data is extracted from a European Commission webservice
+
+__It ONLY works for European countries__
+
+## Requirements ##
+
+PHP with Soap enabled
+
+## Install ##
+
+    composer require elvenpath/yii2-eu-vatvalidator
+
+## Usage ##
+    public function rules()
+    {
+        return [
+            [
+                'vat',
+                EuVatValidator::className(),
+                'country_code' => $this->country_code,
+            ],
+        ];
+    }
+
+You can also populate the model with the company name and address got from the EU database
+
+    public function rules()
+    {
+        return [
+            [
+                'vat',
+                EuVatValidator::className(),
+                'country_code' => $this->country_code,
+                'populate_model' => true,
+                'model_name_attribute' => 'name',
+                'model_address_attribute' => 'address'
+            ],
+        ];
+    }
+
+## Disclaimer ##
+
+Take a look at http://ec.europa.eu/taxation_customs/vies/viesdisc.do to know when/how you're allowed to use this service and his information
